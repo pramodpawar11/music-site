da386e5dbc9098398b7469dd4c4b3610d4e9f8ee
pramod pawar <kingpramodpawar511@gmail.com>
2024-05-08 11:46:42 +0530

    created a footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Whychooseus from "@/components/Whychooseus";
 import { Ourtestimonials } from "@/components/Ourtestimonials";
 import UpcommingWebinars from "@/components/UpcommingWebinars";
 import InstructorSection from "@/components/InstructorSection";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <Ourtestimonials />
         <UpcommingWebinars />
         <InstructorSection/>
+        <Footer/>
       </main>
     </div>
   );
